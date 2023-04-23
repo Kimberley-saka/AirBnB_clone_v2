b7cbf21e0672d986f7ffb1ce08d734b4e7fb7723
Kimberley-saka <kimberleysak@gmail.com>
2023-04-23 06:45:34 -0700

    solution to task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Starts a flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    returns the below string
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    returns HBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_ctext(text_to_display):
+    """
+    display “C ” followed by the value of the text
+    """
+    return "C {}".format(text_to_display.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_python(text_to_display="is cool"):
+    """
+    display python is cool
+    """
+    return "Python {}".format(text_to_display.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
