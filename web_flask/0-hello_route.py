eccd0148a1e89cbd61393b7fe76f31a5f8692f9f
Kimberley-saka <kimberleysak@gmail.com>
2023-04-23 06:10:58 -0700

    web frameworks

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Starts a flask web application
+"""
+from flask import Flask
+
+
+app = flask(__name__)
+
+
+@app.route('/' strict_slashes=False)
+def hello():
+    """
+    returns the below string
+    """
+    return "Hello HBNB!"
+
+
+if __name__ = "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
